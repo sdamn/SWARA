0f492aa6b8fb9122fb5057aa3248e10d014ae5ef
Soham Dandekar <90496440+sdamn@users.noreply.github.com>
2023-12-10 10:41:54 +0530

    Update index.html and script.js

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-const deviceId = document.getElementById('deviceId');
+const deviceIdSelect = document.getElementById('deviceId');
 const apiKey = document.getElementById('apiKey');
 const onBtn = document.getElementById('onBtn');
 const offBtn = document.getElementById('offBtn');
 
 const bodyData = {
-    "request": {
-        "state": 1, // Initial state (ON)
-        "brightness": 0 // Brightness level
-    },
-    "deviceNumber": 3 // Device number
+  "request": {
+    "state": 1, // Initial state (ON)
+    "brightness": 0 // Brightness level
+  },
+  "deviceNumber": 3 // Device number
+};
+
+const bodyDataOff = {
+  "request": {
+    "state": 0 // Turn device off
+  },
+  "deviceNumber": 3 // Device number
 };
 
 function sendRequest(method, url, data) {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Authorization', `Bearer ${apiKey.value}`);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log('Device state updated successfully.');
-            } else {
-                console.error('Error:', xhr.responseText);
-            }
-        }
-    };
-    xhr.send(JSON.stringify(data));
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('Authorization', `Bearer ${apiKey.value}`);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log('Device state updated successfully.');
+      } else {
+        console.error('Error:', xhr.responseText);
+      }
+    }
+  };
+  xhr.send(JSON.stringify(data));
 }
 
 onBtn.addEventListener('click', () => {
-    sendRequest('POST', `https://backend.tinxy.in/v2/devices/${deviceId.value}/toggle`, {
-        ...bodyData, // Spread existing data
-        toggle: 1 // Add toggle property for turning ON
-    });
+  sendRequest('POST', `https://backend.tinxy.in/v2/devices/${deviceIdSelect.value}/toggle`, {
+    ...bodyData, // Spread existing data
+    toggle: 1 // Add toggle property for turning ON
+  });
 });
 
 offBtn.addEventListener('click', () => {
-    sendRequest('POST', `https://backend.tinxy.in/v2/devices/${deviceId.value}/toggle`, {
-        ...bodyData, // Spread existing data
-        toggle: 0 // Add toggle property for turning OFF
-    });
+  sendRequest('POST', `https://backend.tinxy.in/v2/devices/${deviceIdSelect.value}/toggle`, bodyDataOff); // Use specific bodyDataOff object for turning off
 });
